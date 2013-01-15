806ac3d7911fecf282b7c575f7230f9e5008aba2
Troy Shaw <troyshw@gmail.com>
2013-01-16 08:10:03 +1300

    added some file logic into highscore

--- src/highscore.c
+++ src/highscore.c
@@ -6,20 +6,64 @@
 
 HighscoreManager highscoreManager;
 
+
+static const char *FILE_NAME = "hs.dat";
+
 void loadHighscoresFromDisc()
 {
-    //for now we'll just load in dummy data
-    int i;
-    for (i = 0; i < MAX_SCORES; i++)
+    FILE *fp = fopen(FILE_NAME, "r");
+
+    if (fp == NULL)
     {
-        strcpy(highscoreManager.names[i], "-");
-        highscoreManager.scores[i] = 0;
+        //file didn't exist, so load in some default data
+        int i;
+        for (i = 0; i < MAX_SCORES; i++)
+        {
+            strcpy(highscoreManager.names[i], "-");
+            highscoreManager.scores[i] = 0;
+        }
+
+        //then try save it
+        printf("ad");
+        saveHighscoresToDisc();
+        return;
     }
 
+    //now we have to load in our saved data
+
+    int status = fclose(fp);
+
+    if (status != 0)
+    {
+        printf("failed to close file when loading highscore data\n");
+        exit(EXIT_FAILURE);
+    }
 }
 void saveHighscoresToDisc()
 {
+    FILE *fp = fopen(FILE_NAME, "w");
 
+    if (fp == NULL)
+    {
+        printf("failed to open file when saving highscore.\n");
+        printf("highscores will persist in this game, but will be lost if the program is closed.\n");
+        return;
+    }
+
+    int i;
+    for (i = 0; i < MAX_SCORES; i++)
+    {
+        fprintf(fp, highscoreManager.names[i]);
+        fprintf(fp, "\n%d\n", highscoreManager.scores[i]);
+    }
+
+    int status = fclose(fp);
+
+    if (status != 0)
+    {
+        printf("failed to close file when saving highscore data\n");
+        exit(EXIT_FAILURE);
+    }
 }
 void enterScore(char* name, int score)
 {
